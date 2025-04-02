d297f18db2312eb267feced53e197882480ba8b9
danikus31 <58368856+danikus31@users.noreply.github.com>
2025-04-02 14:35:02 +0300

    birthday data comes to class R_user

--- root/add_points/index.php
+++ root/add_points/index.php
@@ -23,41 +23,6 @@ while($randome[$v]){
 }
 */
 
-function was_birthday($user_date){
-    if(date("m",strtotime($user_date)) - date('m') == 0){
-        $test_date = date("d",strtotime($user_date));
-        if($test_date - date(d) <= 0 and $test_date - date(d)>-8){
-            return 1;
-        }
-    }else if(date("m",strtotime($user_date)) - date('m') == -1){
-        $test_date = date("d",strtotime($user_date)) + cal_days_in_month(CAL_GREGORIAN, date('m')-1, date('Y'));
-        if($test_date - date(d) <= 0 and $test_date - date(d)>-8){
-            return 1;
-        }  
-    }else{
-        return 0;
-    }
-}
-
-
-
-
-function is_birthday($user_date){
-    if(date("m",strtotime($user_date)) - date('m') == 0){
-        $test_date = date("d",strtotime($user_date));
-        if($test_date - date(d) <8 and $test_date - date(d)>0){
-            return 1;
-        }
-    }else if(date("m",strtotime($user_date)) - date('m') == 1){
-        $test_date = date("d",strtotime($user_date)) + cal_days_in_month(CAL_GREGORIAN, date('m')-1, date('Y'));
-        if($test_date - date(d) <8 and $test_date - date(d)>0){
-            return 1;
-        }  
-    }else{
-        return 0;
-    }
-}
-
     
 ?>
 
@@ -118,27 +83,18 @@ function is_birthday($user_date){
         </div>
         <div class="birthday_users_wrapper">
         <?php
-        $args = array("have_birthday"=>1);
+        $args = array(
+            "have_birthday"=>true,
+            "max_last_visits_of_users" => 14
+        );
         $birthday_users = new data_displaying($args);
             
         while ($birthday_users->is_data) {
             $birthday_users->load_user();
-            if($birthday_users->user_last_visit< 12){
-                // Check if the birth month is the previous month (adjust to account for current month)
-                if ((int)$birthday_users->user_birth_m + 1 == date("m")) {
-                    $day = (int)$birthday_users->user_birth_d - 29;
-                } 
-                // Check if the birth month is the current month
-                else if ((int)$birthday_users->user_birth_m == date("m")) {
-                    $day = (int)$birthday_users->user_birth_d;
-                }else $day = 10000;
-            
-                // Check if the day difference is between 0 and 7 days
-                if (((int)date("d") - $day >= 0) && ((int)date("d")- $day < 7)) {
-                    echo $birthday_users->user_id . ' ';
-                    echo $birthday_users->user_name . ' ';
-                    echo $birthday_users->user_surname . '<br>';
-                }
+            if($birthday_users->user_has_birthdaysd()){
+                echo $birthday_users->user_id . ' ';
+                echo $birthday_users->user_name . ' ';
+                echo $birthday_users->user_surname . '<br>';
             }
         }
 
@@ -156,29 +112,18 @@ function is_birthday($user_date){
             
         
         <?php
-        $args = array("have_birthday"=>1);
+        $args = array(
+            "have_birthday"=>1,
+            "max_last_visits_of_users" => 14
+        );
         $birthday_users = new data_displaying($args);
             
         while ($birthday_users->is_data) {
             $birthday_users->load_user();
-            
-            if($birthday_users->user_last_visit< 12){
-        
-                // Check if the birth month is the previous month (adjust to account for current month)
-                if ((int)$birthday_users->user_birth_m - 1 == date("m")) {
-                    $day = (int)$birthday_users->user_birth_d + 29;
-                } 
-                // Check if the birth month is the current month
-                else if ((int)$birthday_users->user_birth_m == date("m")) {
-                    $day = (int)$birthday_users->user_birth_d;
-                }else $day = 10000;
-            
-                // Check if the day difference is between 0 and 7 days
-                if (((int)date("d") - $day < 0) && ((int)date("d")- $day > -6)) {
-                    echo $birthday_users->user_id . ' ';
-                    echo $birthday_users->user_name . ' ';
-                    echo $birthday_users->user_surname . '<br>';
-                }
+            if($birthday_users->user_comming_birthday()){
+                echo $birthday_users->user_id . ' ';
+                echo $birthday_users->user_name . ' ';
+                echo $birthday_users->user_surname . '<br>';
             }
         }
         ?>
--- root/includes/R_user.php
+++ root/includes/R_user.php
@@ -1,7 +1,8 @@
 <?php
 
 
-class R_user extends parent_class{
+class R_user extends parent_class
+{
 
     public $user_id;
     public $user_name;
@@ -9,40 +10,86 @@ class R_user extends parent_class{
     public $user_photo;
     public $user_points;
     public $user_last_visit;
-    
+
     public $is_banned;
     public $is_admin;
     public $is_user_present;
-    
+
     //birthday info
     public $is_set_birthday;
     public $user_age;
 
-    public $user_date; 
+    public $user_date;
     public $user_birth_d;
     public $user_birth_m;
     public $user_birth_y;
-
-    public function __construct(... $args){
+    
+    public function __construct(...$args)
+    {
         parent::__construct();
-        
-        if(count($args)===1){
+
+        if (count($args) === 1) {
             $this->get_user_by_position($args[0]);
         }
     }
 
-    public function get_user_by_position($request_position){
+
+
+    public function user_has_birthdaysd()
+    {
+
+        // Check if the birth month is the previous month (adjust to account for current month)
+        if ((int)$this->user_birth_m + 1 == date("m")) {
+            $day = $this->user_birth_d - 29;
+        }
+        // Check if the birth month is the current month
+        else if ($this->user_birth_m == date("m")) {
+            $day = $this->user_birth_d;
+        } else
+            $day = 10000;
+
+        // Check if the day difference is between 0 and 7 days
+        if (((int) date("d") - $day >= 0) && ((int) date("d") - $day < 7)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function user_comming_birthday()
+    {
+        // Check if the birth month is the previous month (adjust to account for current month)
+        if ($this->user_birth_m - 1 == date("m")) {
+            $day = $this->user_birth_d + 29;
+        }
+        // Check if the birth month is the current month
+        else if ((int) $this->user_birth_m == date("m")) {
+            $day = (int) $this->user_birth_d;
+        } else
+            $day = 10000;
+
+        // Check if the day difference is between 0 and 7 days
+        if (( date("d") - $day < 0) && ( date("d") - $day > -8)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public function get_user_by_position($request_position)
+    {
         $this->user_id = $this->data_users[$request_position]['id'];
 
 
         $this->is_user_present = in_array($this->user_id, $this->today_visits_list);
 
-        
+
         $this->user_name = $this->data_users[$request_position]['name'];
         $this->user_surname = $this->data_users[$request_position]['surname'];
-        $this->user_photo = $this->url_to_photo.$this->user_id.".jpg";
+        $this->user_photo = $this->url_to_photo . $this->user_id . ".jpg";
         $this->user_points = $this->data_users[$request_position]['points'];
-        
+
 
         //find last visit
         $this->user_last_visit = 0;
@@ -56,31 +103,31 @@ class R_user extends parent_class{
         }
         $this->is_banned = isset($this->data_users[$request_position]['banned']);
 
-        if(isset($this->data_users[$request_position]['admin'])){
+        if (isset($this->data_users[$request_position]['admin'])) {
             $this->is_admin = $this->data_users[$request_position]['admin'];
-        }else{
+        } else {
             $this->is_admin = false;
         }
 
 
         $this->is_set_birthday = isset($this->data_users[$request_position]['birth']);
-        
-        if($this->is_set_birthday){
-            $this->user_birth_d = date("d",strtotime($this->data_users[$request_position]['birth']));
-            $this->user_birth_m = date("m",strtotime($this->data_users[$request_position]['birth']));
-            $this->user_birth_y = date("Y",strtotime($this->data_users[$request_position]['birth']));
+
+        if ($this->is_set_birthday) {
+            $this->user_birth_d = date("d", strtotime($this->data_users[$request_position]['birth']));
+            $this->user_birth_m = date("m", strtotime($this->data_users[$request_position]['birth']));
+            $this->user_birth_y = date("Y", strtotime($this->data_users[$request_position]['birth']));
             $this->user_date = $this->data_users[$request_position]['birth'];
-            
-            
-            $this->user_age = date('Y') - date("Y",strtotime($this->user_date));
-            if(date('m') < date("m",strtotime($this->user_date))){
+
+
+            $this->user_age = date('Y') - date("Y", strtotime($this->user_date));
+            if (date('m') < date("m", strtotime($this->user_date))) {
                 $this->user_age--;
-            }else if(date('m') == date("m",strtotime($this->user_date))){
-                if(date('d') < date("d",strtotime($this->user_date))){
+            } else if (date('m') == date("m", strtotime($this->user_date))) {
+                if (date('d') < date("d", strtotime($this->user_date))) {
                     $this->user_age--;
                 }
             }
-        }else{
+        } else {
             $this->user_birth_d = '';
             $this->user_birth_m = '';
             $this->user_birth_y = '';
@@ -92,52 +139,54 @@ class R_user extends parent_class{
 
 
 
-    public function today_add_point(){
-        if(!in_array($this->user_id,  $this->today_visits_list)){
+    public function today_add_point()
+    {
+        if (!in_array($this->user_id, $this->today_visits_list)) {
 
             $this->user_points++;
             $this->today_visits_count++;
-            
+
             //adding point to data
             $this->data_users[$this->user_id]['points']++;
 
             //adding to visit list
-            $this->data_visits[count($this->data_visits)-1]['list'] [count($this->data_visits[count($this->data_visits)-1]['list'])] = $this->user_id;
-           
+            $this->data_visits[count($this->data_visits) - 1]['list'][count($this->data_visits[count($this->data_visits) - 1]['list'])] = $this->user_id;
+
             //adding to today randome
-            $this->data_randome[count($this->data_randome)][0]=$this->user_id;
-            $this->data_randome[count($this->data_randome)-1][1]=0;
+            $this->data_randome[count($this->data_randome)][0] = $this->user_id;
+            $this->data_randome[count($this->data_randome) - 1][1] = 0;
 
             //var_dump($this->data_randome);
-           
+
             $this->data_save_all();
         }
     }
 
-    public function today_substract_point(){
-        if(in_array($this->user_id,  $this->data_visits)){
+    public function today_substract_point()
+    {
+        if (in_array($this->user_id, $this->data_visits)) {
 
             $this->user_points--;
             $this->today_visits_count--;
-            
+
             //substracting point from data
             $this->data_users[$this->user_id]['points']--;
-            
+
             //substracting from today visits
-            $visits_key = array_search($this->user_id, $this->data_visits[count($this->data_visits)-1] ['list']);
-            array_splice($this->data_visits[count($this->data_visits)-1] ['list'], $visits_key,1);
-            
+            $visits_key = array_search($this->user_id, $this->data_visits[count($this->data_visits) - 1]['list']);
+            array_splice($this->data_visits[count($this->data_visits) - 1]['list'], $visits_key, 1);
 
             //substracting from today randome
-            $user_key = array_search($this->user_id, haystack: array_column($this->data_randome , 0));
-            array_splice($this->data_randome, $user_key,1);
-            
+            $user_key = array_search($this->user_id, haystack: array_column($this->data_randome, 0));
+            array_splice($this->data_randome, $user_key, 1);
+
             $this->data_save_all();
         }
     }
 
-    public function update_birthday($date){
-        $this->data_users[$this->user_id]['birth']=$date;
+    public function update_birthday($date)
+    {
+        $this->data_users[$this->user_id]['birth'] = $date;
         $this->data_save_all();
     }
 }
